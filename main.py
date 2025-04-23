4450a8a54d5922efff8d411a05e20fcb9b7fa98d
ZofiaLor <zosia984@gmail.com>
2025-04-23 19:58:49 +0200

    Simple clustering of iris dataset

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
+import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.cluster import AgglomerativeClustering
+from scipy.cluster.hierarchy import dendrogram, linkage
+from fuzzy_cmeans import FuzzyCMeans as FCM
+import seaborn as sns
 import random
 
-N = 50
+# load the data and convert it to an appropriate format
+iris = load_iris()
+x = iris.data[:, 2].tolist()
+y = iris.data[:, 3].tolist()
+data = np.array(list(zip(x, y)))
 
-x = [random.randint(0, 70) for i in range(N)]
-y = [random.randint(20, 90) for i in range(N)]
-a = [random.randint(50, 99) for i in range(N)]
-b = [random.randint(0, 30) for i in range(N)]
+# perform hierarchical clustering and display the results
+cluster = AgglomerativeClustering(n_clusters=2)
+labels = cluster.fit_predict(data)
+plt.scatter(x, y, c=labels)
+plt.show()
 
-plt.scatter(x, y, c=[x[i] * y[i] for i in range(N)])
-plt.scatter(a, b, c=[a[i] * b[i] for i in range(N)], cmap="cool")
+# display a dendrogram visualizing the clustering
+dendro = linkage(data, method='ward', metric='euclidean')
+dendrogram(dendro)
+plt.show()
+
+# perform fuzzy c-means clustering and display the results
+fcm = FCM(n_clusters=10)
+fcm.fit(data)
+labels_clusters = fcm.predict(data)
+plt.scatter(x, y, c=labels_clusters)
+plt.scatter(fcm.cluster_centers_[:, 0], fcm.cluster_centers_[:, 1], c='red')
 plt.show()
 
